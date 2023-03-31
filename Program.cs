7a7732d4000a81a219f2b55b08a09160776712c8
vuductho96 <112962875+vuductho96@users.noreply.github.com>
2023-03-31 23:30:08 +0700

    UngDungDoiTien

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UsdToVNd
+{
+    class UsdToVNd
+    {
+        static void Main(string[] args)
+        {
+            const int Rate = 23000;
+
+            Console.Write("Enter the amount of USD you have: ");
+            float USD = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("You entered: " + USD.ToString("C") + " USD");
+
+            float Result = USD * Rate;
+            Console.WriteLine("The amount in VND is: " + Result.ToString("C") + " VND");
+        }
+    }
+}
+
